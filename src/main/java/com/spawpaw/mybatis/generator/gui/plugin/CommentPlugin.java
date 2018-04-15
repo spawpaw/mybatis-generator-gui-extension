e7199d38c37eacc7671f5bbac882560475dca923
BenBenShang~ <spawpaw@hotmail.com>
2018-04-15 14:37:40 +0800

    feat: add `@mbg.generated` for generated codes ( #11 #14 #16 ) - the code who has a `@mbg.generated` comment will be deleted when regenerating codes. - there is a switch to open/close it.

--- src/main/java/com/spawpaw/mybatis/generator/gui/plugin/CommentPlugin.java
+++ src/main/java/com/spawpaw/mybatis/generator/gui/plugin/CommentPlugin.java
@@ -4,9 +4,17 @@ import com.spawpaw.mybatis.generator.gui.util.Utils;
 import org.mybatis.generator.api.CommentGenerator;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.api.dom.java.*;
+import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
+import org.mybatis.generator.config.MergeConstants;
+import org.mybatis.generator.internal.util.StringUtility;
 
+import javax.xml.bind.DatatypeConverter;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Properties;
 import java.util.Set;
 
@@ -18,23 +26,340 @@ import java.util.Set;
  * @author BenBenShang spawpaw@hotmail.com
  */
 public class CommentPlugin implements CommentGenerator {
+
     public boolean generateJPA = false;
+    public boolean suppressDate = false;
+    public String dateFormat;
     public String fileHeader = "";
+    public boolean enableMbgGenerated = true;
+    public boolean enableCorrespondingTable = true;
 
     @Override
     public void addConfigurationProperties(Properties properties) {
         Utils.injectFieldsFromProperties(this, properties);
-        System.out.println("file header");
-        System.out.println(fileHeader);
     }
 
     @Override
-    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
-        if (introspectedColumn.getRemarks() != null && !introspectedColumn.getRemarks().isEmpty()) {
-            field.addJavaDocLine("/**");
-            field.addJavaDocLine("* " + introspectedColumn.getRemarks().replace("\n", "\n* "));
-            field.addJavaDocLine("**/");
-        } else System.out.printf("字段%s没有注释，将不为field添加注释\n", introspectedColumn.getActualColumnName());
+    public void addJavaFileComment(CompilationUnit compilationUnit) {
+        if (generateJPA) {
+            compilationUnit.addImportedType(new FullyQualifiedJavaType("org.hibernate.validator.constraints.NotEmpty"));
+            compilationUnit.addImportedType(new FullyQualifiedJavaType("javax.persistence.Table"));
+            compilationUnit.addImportedType(new FullyQualifiedJavaType("javax.persistence.GeneratedValue"));
+            compilationUnit.addImportedType(new FullyQualifiedJavaType("javax.persistence.Id"));
+            compilationUnit.addImportedType(new FullyQualifiedJavaType("javax.persistence.Column"));
+        }
+    }
+
+    /**
+     * Adds a suitable comment to warn users that the element was generated, and when it was generated.
+     *
+     * @param xmlElement the xml element
+     */
+    @Override
+    public void addComment(XmlElement xmlElement) {
+        if (!enableMbgGenerated) return;
+        xmlElement.addElement(new TextElement("<!--")); //$NON-NLS-1$
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("  WARNING - "); //$NON-NLS-1$
+        sb.append(MergeConstants.NEW_ELEMENT_TAG);
+        String s = getDateString();
+        if (s != null) {
+//            sb.setLength(0);
+            sb.append("  This element was generated on "); //$NON-NLS-1$
+            sb.append(s);
+            sb.append('.');
+            sb.append(" by MyBatis Generator, do not modify.");
+        }
+        xmlElement.addElement(new TextElement(sb.toString()));
+        xmlElement.addElement(new TextElement("-->")); //$NON-NLS-1$
+    }
+
+    @Override
+    public void addRootComment(XmlElement rootElement) {
+        // add no document level comments by default
+    }
+
+    /**
+     * This method adds the custom javadoc tag for. You may do nothing if you do not wish to include the Javadoc tag -
+     * however, if you do not include the Javadoc tag then the Java merge capability of the eclipse plugin will break.
+     *
+     * @param javaElement       the java element
+     * @param markAsDoNotDelete the mark as do not delete
+     */
+    protected void addJavadocTag(JavaElement javaElement,
+                                 boolean markAsDoNotDelete) {
+        if (!enableMbgGenerated) return;
+        javaElement.addJavaDocLine(" *"); //$NON-NLS-1$
+        StringBuilder sb = new StringBuilder();
+        sb.append(" * "); //$NON-NLS-1$
+        sb.append(MergeConstants.NEW_ELEMENT_TAG);
+        if (markAsDoNotDelete) {
+            sb.append(" do_not_delete_during_merge"); //$NON-NLS-1$
+        }
+        String s = getDateString();
+        if (s != null) {
+            sb.append(' ');
+            sb.append(s);
+        }
+        javaElement.addJavaDocLine(sb.toString());
+    }
+
+    /**
+     * Returns a formated date string to include in the Javadoc tag
+     * and XML comments. You may return null if you do not want the date in
+     * these documentation elements.
+     *
+     * @return a string representing the current timestamp, or null
+     */
+    protected String getDateString() {
+        SimpleDateFormat dateFormat = null;
+        String dateFormatString = this.dateFormat;
+        if (StringUtility.stringHasValue(dateFormatString)) {
+            dateFormat = new SimpleDateFormat(dateFormatString);
+        }
+
+        if (!(dateFormatString == null || dateFormatString.isEmpty()))
+            dateFormat = new SimpleDateFormat(dateFormatString);
+        if (suppressDate) {
+            return null;
+        } else if (dateFormat != null) {
+            return dateFormat.format(new Date());
+        } else {
+            return new Date().toString();
+        }
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass,
+                                IntrospectedTable introspectedTable) {
+        StringBuilder sb = new StringBuilder();
+
+        innerClass.addJavaDocLine("/**"); //$NON-NLS-1$
+        if (enableCorrespondingTable) {
+            sb.append(" * Corresponding to the database table "); //$NON-NLS-1$
+            sb.append(introspectedTable.getFullyQualifiedTable());
+            innerClass.addJavaDocLine(sb.toString());
+        }
+
+        addJavadocTag(innerClass, false);
+
+        innerClass.addJavaDocLine(" */"); //$NON-NLS-1$
+    }
+
+    @Override
+    public void addClassComment(InnerClass innerClass,
+                                IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
+        StringBuilder sb = new StringBuilder();
+
+        innerClass.addJavaDocLine("/**"); //$NON-NLS-1$
+        if (enableCorrespondingTable) {
+            sb.append(" * Corresponding to the database table "); //$NON-NLS-1$
+            sb.append(introspectedTable.getFullyQualifiedTable());
+            innerClass.addJavaDocLine(sb.toString());
+        }
+
+        addJavadocTag(innerClass, markAsDoNotDelete);
+
+        innerClass.addJavaDocLine(" */"); //$NON-NLS-1$
+    }
+
+    @Override
+    public void addModelClassComment(TopLevelClass topLevelClass,
+                                     IntrospectedTable introspectedTable) {
+        if (fileHeader != null && !fileHeader.isEmpty() && introspectedTable.getRemarks() != null) {
+            fileHeader = fileHeader.replace("${tableComment}", introspectedTable.getRemarks());
+            topLevelClass.addJavaDocLine(fileHeader);
+        }
+        if (generateJPA) {
+            topLevelClass.addAnnotation("@Table(name=\"" + introspectedTable.getFullyQualifiedTableNameAtRuntime() + "\")");
+        }
+    }
+
+    @Override
+    public void addEnumComment(InnerEnum innerEnum,
+                               IntrospectedTable introspectedTable) {
+
+        StringBuilder sb = new StringBuilder();
+
+        innerEnum.addJavaDocLine("/**"); //$NON-NLS-1$
+//        innerEnum
+//                .addJavaDocLine(" * This enum was generated by MyBatis Generator."); //$NON-NLS-1$
+
+//        sb.append(" * This enum corresponds to the database table "); //$NON-NLS-1$
+        if (enableCorrespondingTable) {
+            sb.append(" * corresponding to the database table "); //$NON-NLS-1$
+            sb.append(introspectedTable.getFullyQualifiedTable());
+            innerEnum.addJavaDocLine(sb.toString());
+        }
+
+        addJavadocTag(innerEnum, false);
+
+        innerEnum.addJavaDocLine(" */"); //$NON-NLS-1$
+    }
+
+    @Override
+    public void addFieldComment(Field field,
+                                IntrospectedTable introspectedTable,
+                                IntrospectedColumn introspectedColumn) {
+        field.addJavaDocLine("/**"); //$NON-NLS-1$
+
+        String remarks = introspectedColumn.getRemarks();
+        if (StringUtility.stringHasValue(remarks)) {
+            String[] remarkLines = remarks.split(System.getProperty("line.separator"));  //$NON-NLS-1$
+            for (String remarkLine : remarkLines) {
+                field.addJavaDocLine(" *   " + remarkLine);  //$NON-NLS-1$
+            }
+        }
+
+        field.addJavaDocLine(" *"); //$NON-NLS-1$
+        StringBuilder sb = new StringBuilder();
+        if (enableCorrespondingTable) {
+            sb.append(" * Corresponding to the database column "); //$NON-NLS-1$
+            sb.append(introspectedTable.getFullyQualifiedTable());
+            sb.append('.');
+            sb.append(introspectedColumn.getActualColumnName());
+            field.addJavaDocLine(sb.toString());
+        }
+
+        addJavadocTag(field, false);
+
+        field.addJavaDocLine(" */"); //$NON-NLS-1$
+
+
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
+
+        StringBuilder sb = new StringBuilder();
+
+        field.addJavaDocLine("/**"); //$NON-NLS-1$
+
+        if (enableCorrespondingTable) {
+            sb.append(" * Corresponding to the database table "); //$NON-NLS-1$
+            sb.append(introspectedTable.getFullyQualifiedTable());
+            field.addJavaDocLine(sb.toString());
+        }
+
+        addJavadocTag(field, false);
+
+        field.addJavaDocLine(" */"); //$NON-NLS-1$
+    }
+
+    @Override
+    public void addGeneralMethodComment(Method method,
+                                        IntrospectedTable introspectedTable) {
+
+        method.addJavaDocLine("/**"); //$NON-NLS-1$
+
+//        StringBuilder sb = new StringBuilder();
+//        sb.append(" * This method corresponds to the database table "); //$NON-NLS-1$
+//        sb.append(introspectedTable.getFullyQualifiedTable());
+//        method.addJavaDocLine(sb.toString());
+
+        addJavadocTag(method, false);
+
+        method.addJavaDocLine(" */"); //$NON-NLS-1$
+    }
+
+    @Override
+    public void addGetterComment(Method method,
+                                 IntrospectedTable introspectedTable,
+                                 IntrospectedColumn introspectedColumn) {
+
+        StringBuilder sb = new StringBuilder();
+
+        method.addJavaDocLine("/**"); //$NON-NLS-1$
+
+        sb.append(" * This method returns the value of the database column "); //$NON-NLS-1$
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
+        sb.append(introspectedColumn.getActualColumnName());
+        method.addJavaDocLine(sb.toString());
+
+        method.addJavaDocLine(" *"); //$NON-NLS-1$
+
+        sb.setLength(0);
+        sb.append(" * @return the value of "); //$NON-NLS-1$
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
+        sb.append(introspectedColumn.getActualColumnName());
+        method.addJavaDocLine(sb.toString());
+
+        addJavadocTag(method, false);
+
+        method.addJavaDocLine(" */"); //$NON-NLS-1$
+    }
+
+    @Override
+    public void addSetterComment(Method method,
+                                 IntrospectedTable introspectedTable,
+                                 IntrospectedColumn introspectedColumn) {
+
+        StringBuilder sb = new StringBuilder();
+
+        method.addJavaDocLine("/**"); //$NON-NLS-1$
+
+        sb.append(" * This method sets the value of the database column "); //$NON-NLS-1$
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
+        sb.append(introspectedColumn.getActualColumnName());
+        method.addJavaDocLine(sb.toString());
+
+        method.addJavaDocLine(" *"); //$NON-NLS-1$
+
+        Parameter parm = method.getParameters().get(0);
+        sb.setLength(0);
+        sb.append(" * @param "); //$NON-NLS-1$
+        sb.append(parm.getName());
+        sb.append(" the value for "); //$NON-NLS-1$
+        sb.append(introspectedTable.getFullyQualifiedTable());
+        sb.append('.');
+        sb.append(introspectedColumn.getActualColumnName());
+        method.addJavaDocLine(sb.toString());
+
+        addJavadocTag(method, false);
+
+        method.addJavaDocLine(" */"); //$NON-NLS-1$
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable,
+                                           Set<FullyQualifiedJavaType> imports) {
+        imports.add(new FullyQualifiedJavaType("javax.annotation.Generated")); //$NON-NLS-1$
+        String comment = "Source Table: " + introspectedTable.getFullyQualifiedTable().toString(); //$NON-NLS-1$
+        method.addAnnotation(getGeneratedAnnotation(comment));
+    }
+
+    @Override
+    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable,
+                                           IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+        imports.add(new FullyQualifiedJavaType("javax.annotation.Generated")); //$NON-NLS-1$
+        String comment = "Source field: " //$NON-NLS-1$
+                + introspectedTable.getFullyQualifiedTable().toString()
+                + "." //$NON-NLS-1$
+                + introspectedColumn.getActualColumnName();
+        method.addAnnotation(getGeneratedAnnotation(comment));
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable,
+                                   Set<FullyQualifiedJavaType> imports) {
+        imports.add(new FullyQualifiedJavaType("javax.annotation.Generated")); //$NON-NLS-1$
+        String comment = "Source Table: " + introspectedTable.getFullyQualifiedTable().toString(); //$NON-NLS-1$
+        field.addAnnotation(getGeneratedAnnotation(comment));
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable,
+                                   IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> imports) {
+        imports.add(new FullyQualifiedJavaType("javax.annotation.Generated")); //$NON-NLS-1$
+        String comment = "Source field: " //$NON-NLS-1$
+                + introspectedTable.getFullyQualifiedTable().toString()
+                + "." //$NON-NLS-1$
+                + introspectedColumn.getActualColumnName();
+        field.addAnnotation(getGeneratedAnnotation(comment));
         if (generateJPA) {
             boolean isId = false;
             for (IntrospectedColumn column : introspectedTable.getPrimaryKeyColumns()) {
@@ -61,93 +386,33 @@ public class CommentPlugin implements CommentGenerator {
     }
 
     @Override
-    public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
-
+    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable,
+                                   Set<FullyQualifiedJavaType> imports) {
+        imports.add(new FullyQualifiedJavaType("javax.annotation.Generated")); //$NON-NLS-1$
+        String comment = "Source Table: " + introspectedTable.getFullyQualifiedTable().toString(); //$NON-NLS-1$
+        innerClass.addAnnotation(getGeneratedAnnotation(comment));
     }
 
-    @Override
-    public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        if (fileHeader != null && !fileHeader.isEmpty() && introspectedTable.getRemarks() != null) {
-            fileHeader = fileHeader.replace("${tableComment}", introspectedTable.getRemarks());
-            topLevelClass.addJavaDocLine(fileHeader);
+    private String getGeneratedAnnotation(String comment) {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append("@Generated("); //$NON-NLS-1$
+        buffer.append("value=\""); //$NON-NLS-1$
+
+        buffer.append(MyBatisGenerator.class.getName());
+        buffer.append('\"');
+
+        if (!suppressDate) {
+            buffer.append(", date=\""); //$NON-NLS-1$
+            buffer.append(DatatypeConverter.printDateTime(Calendar.getInstance()));
+            buffer.append('\"');
         }
-        if (generateJPA) {
-            topLevelClass.addAnnotation("@Table(name=\"" + introspectedTable.getFullyQualifiedTableNameAtRuntime() + "\")");
-        }
+
+        buffer.append(", comments=\""); //$NON-NLS-1$
+        buffer.append(comment);
+        buffer.append('\"');
+
+        buffer.append(')');
+        return buffer.toString();
     }
 
-    @Override
-    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
-    }
-
-    @Override
-    public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean markAsDoNotDelete) {
-
-    }
-
-    @Override
-    public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
-
-    }
-
-    @Override
-    public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
-
-    }
-
-    @Override
-    public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
-
-    }
-
-    @Override
-    public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
-
-    }
-
-    @Override
-    public void addJavaFileComment(CompilationUnit compilationUnit) {
-        if (generateJPA) {
-            compilationUnit.addImportedType(new FullyQualifiedJavaType("org.hibernate.validator.constraints.NotEmpty"));
-            compilationUnit.addImportedType(new FullyQualifiedJavaType("javax.persistence.Table"));
-            compilationUnit.addImportedType(new FullyQualifiedJavaType("javax.persistence.GeneratedValue"));
-            compilationUnit.addImportedType(new FullyQualifiedJavaType("javax.persistence.Id"));
-            compilationUnit.addImportedType(new FullyQualifiedJavaType("javax.persistence.Column"));
-        }
-    }
-
-    @Override
-    public void addComment(XmlElement xmlElement) {
-
-    }
-
-    @Override
-    public void addRootComment(XmlElement rootElement) {
-
-    }
-
-    @Override
-    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
-
-    }
-
-    @Override
-    public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
-
-    }
-
-    @Override
-    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
-
-    }
-
-    @Override
-    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
-
-    }
-
-    @Override
-    public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
-
-    }
 }
